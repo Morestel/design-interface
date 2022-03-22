fc9adc872bd2f697e5f96a29a681677ac21a48eb
Morestel <mystkun6@gmail.com>
2022-03-22 18:05:36 +0100

    Add the current month and the list of the days for this month

--- src/components/MonthDate.vue
+++ src/components/MonthDate.vue
@@ -1,4 +1,46 @@
 <template>
     
-    
+{{ month }}
+
+<ul>
+    <li v-for="day in this.days" :key="day">
+        {{ day }}
+    </li>
+</ul>
 </template>
+
+<script>
+export default{
+    data: () => ({
+        date: new Date(),
+        month: '',
+        days: [],
+        monthInYear: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+    }),
+
+    mounted(){
+        this.extractDate()
+        this.getDaysInMonth(this.date.getMonth(), this.date.getFullYear())
+    },
+
+    methods:{
+        extractDate: function(){
+            this.month = this.monthInYear[this.date.getMonth()]
+        },
+
+        getDaysInMonth: function(month, year) {
+            console.log(month)
+            var date = new Date(year, month, 1);
+            
+            while (date.getMonth() === month) {
+                this.days.push(new Date(date).getDate());
+                date.setDate(date.getDate() + 1);
+            }
+        }
+    }
+}
+
+
+</script>
+
