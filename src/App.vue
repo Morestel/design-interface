6db40e24a315a9eedaf3bc058b301301225c567d
Morestel <mystkun6@gmail.com>
2022-03-22 19:18:02 +0100

    Creation of the TaskCard with SVG

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import TheWelcome from './components/TheWelcome.vue'
 import ProfilePictureVue from './components/ProfilePicture.vue';
 import MonthDateVue from './components/MonthDate.vue';
 import TaskCardVue from './components/TaskCard.vue';
+import TaskCard from './components/TaskCard.vue';
 </script>
 
 <template>
@@ -13,8 +14,55 @@ import TaskCardVue from './components/TaskCard.vue';
 
   <main>
     <MonthDateVue />
+    <svg id="svgBlock" width="100%" height="1500px">
+
+      <foreignObject v-for="h in hours" :key="h.t" v-bind:x="h.x" v-bind:y="h.y" v-bind:width="h.w" v-bind:height="h.h">
+            <div>{{h.t}}</div>
+        </foreignObject>
+     
+
+      <TaskCard v-for="item in listItem" :key="item" :item="item"/>
+    </svg>
+    
   </main>
 </template>
+<script>
+export default{
+    data: () => ({
+        hours: [{ t: "00:00", x: "0", y: "60", w: "12.5%", h: "60px" },
+                    { t: "01:00", x: "0", y: "120", w: "12.5%", h: "60px" },
+                    { t: "02:00", x: "0", y: "180", w: "12.5%", h: "60px" },
+                    { t: "03:00", x: "0", y: "240", w: "12.5%", h: "60px" },
+                    { t: "04:00", x: "0", y: "300", w: "12.5%", h: "60px" },
+                    { t: "05:00", x: "0", y: "360", w: "12.5%", h: "60px" },
+                    { t: "06:00", x: "0", y: "420", w: "12.5%", h: "60px" },
+                    { t: "07:00", x: "0", y: "480", w: "12.5%", h: "60px" },
+                    { t: "08:00", x: "0", y: "540", w: "12.5%", h: "60px" },
+                    { t: "09:00", x: "0", y: "600", w: "12.5%", h: "60px" },
+                    { t: "10:00", x: "0", y: "660", w: "12.5%", h: "60px" },
+                    { t: "11:00", x: "0", y: "720", w: "12.5%", h: "60px" },
+                    { t: "12:00", x: "0", y: "780", w: "12.5%", h: "60px" },
+                    { t: "13:00", x: "0", y: "840", w: "12.5%", h: "60px" },
+                    { t: "14:00", x: "0", y: "900", w: "12.5%", h: "60px" },
+                    { t: "15:00", x: "0", y: "960", w: "12.5%", h: "60px" },
+                    { t: "16:00", x: "0", y: "1020", w: "12.5%", h: "60px" },
+                    { t: "17:00", x: "0", y: "1080", w: "12.5%", h: "60px" },
+                    { t: "18:00", x: "0", y: "1140", w: "12.5%", h: "60px" },
+                    { t: "19:00", x: "0", y: "1200", w: "12.5%", h: "60px" },
+                    { t: "20:00", x: "0", y: "1260", w: "12.5%", h: "60px" },
+                    { t: "21:00", x: "0", y: "1320", w: "12.5%", h: "60px" },
+                    { t: "22:00", x: "0", y: "1380", w: "12.5%", h: "60px" },
+                    { t: "23:00", x: "0", y: "1440", w: "12.5%", h: "60px" },],
+        listItem: [{"id":1,"date":"12/02/2022","start-time":"09:00","end-time":"10:00","title":"Mobile app design","members":[{"name":"Mike","avatar_url":"https://reqres.in/img/faces/3-image.jpg",},
+{"name":"Anita","avatar_url":"https://reqres.in/img/faces/3-image.jpg",},]}],
+    }),
+    methods:{
+        
+    }
+}
+
+
+</script>
 
 <style>
 @import './assets/base.css';
--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,3 +1,53 @@
+<script setup>
+defineProps({
+  item: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
 <template>
-    
+    <foreignObject :x="100" :y="svgY(item)" width="40%" :height="svgHeight(item)">
+        <div>{{item.title}}</div>
+    </foreignObject>
 </template>
+
+<script>
+export default{
+    data: () => ({
+        
+    }),
+
+    mounted(){
+        console.log(this.item)
+    },
+
+    methods:{
+
+        svgY: function (item) { // In function of the start of the item
+            let hour = item["start-time"].slice(0, 2);
+            let minutes = item["start-time"].slice(3, 5);
+            return "" + (60 + (hour * 60) + minutes * 1);
+        },
+
+        svgHeight: function (item) {
+
+            let hourBegin = item["start-time"].slice(0, 2);
+            let minutesBegin = item["start-time"].slice(3, 5);
+
+            let hourEnd = item["end-time"].slice(0, 2);
+            let minutesEnd = item["end-time"].slice(3, 5);
+            // Starting by convert hour in minutes then we add the minutes
+            let timeHourBegin = hourBegin * 60 + minutesBegin; // 600 
+            let timeHourEnd = hourEnd * 60 + minutesEnd;  // 720
+            // Then we do the difference between them and we return it
+            return "" + timeHourEnd - timeHourBegin;
+        },
+
+    }
+}
+
+
+</script>
+
