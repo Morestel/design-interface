8c2037ae76aed3b669d77c03f71bdb6d845486ba
Morestel <mystkun6@gmail.com>
2022-03-25 19:05:48 +0100

    Update of the line of time

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,15 @@ import Common from './components/Common.vue';
     
   <main>
     <MonthDateVue @changeDate="changeDate($event)" />
-    <svg id="svgBlock"  width="70%" height="1500px">
+    <svg id="svgBlock" width="70%" height="1500px">
 
       <foreignObject v-for="h in hours" :key="h.t" v-bind:x="h.x" v-bind:y="h.y" v-bind:width="h.w" v-bind:height="h.h" class="hour">
             <div>{{h.t}}</div>
       </foreignObject>
      
+      <foreignObject x="5%" :y="timeLine" width="100%" height="1px" id="time-actual">
+
+      </foreignObject>
 
       <TaskCard v-for="task in this.listItem" :key="task" :item="task" @changeCommon="changeCommon($event)"/>
     </svg>
@@ -32,38 +35,41 @@ import Common from './components/Common.vue';
 <script>
 export default{
     data: () => ({
-        hours: [{ t: "00:00", x: "0", y: "60", w: "12.5%", h: "60px" },
-                    { t: "01:00", x: "0", y: "120", w: "12.5%", h: "60px" },
-                    { t: "02:00", x: "0", y: "180", w: "12.5%", h: "60px" },
-                    { t: "03:00", x: "0", y: "240", w: "12.5%", h: "60px" },
-                    { t: "04:00", x: "0", y: "300", w: "12.5%", h: "60px" },
-                    { t: "05:00", x: "0", y: "360", w: "12.5%", h: "60px" },
-                    { t: "06:00", x: "0", y: "420", w: "12.5%", h: "60px" },
-                    { t: "07:00", x: "0", y: "480", w: "12.5%", h: "60px" },
-                    { t: "08:00", x: "0", y: "540", w: "12.5%", h: "60px" },
-                    { t: "09:00", x: "0", y: "600", w: "12.5%", h: "60px" },
-                    { t: "10:00", x: "0", y: "660", w: "12.5%", h: "60px" },
-                    { t: "11:00", x: "0", y: "720", w: "12.5%", h: "60px" },
-                    { t: "12:00", x: "0", y: "780", w: "12.5%", h: "60px" },
-                    { t: "13:00", x: "0", y: "840", w: "12.5%", h: "60px" },
-                    { t: "14:00", x: "0", y: "900", w: "12.5%", h: "60px" },
-                    { t: "15:00", x: "0", y: "960", w: "12.5%", h: "60px" },
-                    { t: "16:00", x: "0", y: "1020", w: "12.5%", h: "60px" },
-                    { t: "17:00", x: "0", y: "1080", w: "12.5%", h: "60px" },
-                    { t: "18:00", x: "0", y: "1140", w: "12.5%", h: "60px" },
-                    { t: "19:00", x: "0", y: "1200", w: "12.5%", h: "60px" },
-                    { t: "20:00", x: "0", y: "1260", w: "12.5%", h: "60px" },
-                    { t: "21:00", x: "0", y: "1320", w: "12.5%", h: "60px" },
-                    { t: "22:00", x: "0", y: "1380", w: "12.5%", h: "60px" },
-                    { t: "23:00", x: "0", y: "1440", w: "12.5%", h: "60px" },],
+        hours: [{ t: "00:00", x: "0", y: "0", w: "12.5%", h: "60px" },
+                    { t: "01:00", x: "0", y: "60", w: "12.5%", h: "60px" },
+                    { t: "02:00", x: "0", y: "120", w: "12.5%", h: "60px" },
+                    { t: "03:00", x: "0", y: "180", w: "12.5%", h: "60px" },
+                    { t: "04:00", x: "0", y: "240", w: "12.5%", h: "60px" },
+                    { t: "05:00", x: "0", y: "300", w: "12.5%", h: "60px" },
+                    { t: "06:00", x: "0", y: "360", w: "12.5%", h: "60px" },
+                    { t: "07:00", x: "0", y: "420", w: "12.5%", h: "60px" },
+                    { t: "08:00", x: "0", y: "480", w: "12.5%", h: "60px" },
+                    { t: "09:00", x: "0", y: "540", w: "12.5%", h: "60px" },
+                    { t: "10:00", x: "0", y: "600", w: "12.5%", h: "60px" },
+                    { t: "11:00", x: "0", y: "660", w: "12.5%", h: "60px" },
+                    { t: "12:00", x: "0", y: "720", w: "12.5%", h: "60px" },
+                    { t: "13:00", x: "0", y: "780", w: "12.5%", h: "60px" },
+                    { t: "14:00", x: "0", y: "840", w: "12.5%", h: "60px" },
+                    { t: "15:00", x: "0", y: "900", w: "12.5%", h: "60px" },
+                    { t: "16:00", x: "0", y: "960", w: "12.5%", h: "60px" },
+                    { t: "17:00", x: "0", y: "1020", w: "12.5%", h: "60px" },
+                    { t: "18:00", x: "0", y: "1080", w: "12.5%", h: "60px" },
+                    { t: "19:00", x: "0", y: "1140", w: "12.5%", h: "60px" },
+                    { t: "20:00", x: "0", y: "1200", w: "12.5%", h: "60px" },
+                    { t: "21:00", x: "0", y: "1260", w: "12.5%", h: "60px" },
+                    { t: "22:00", x: "0", y: "1320", w: "12.5%", h: "60px" },
+                    { t: "23:00", x: "0", y: "1380", w: "12.5%", h: "60px" },],
         data: [],
         listItem :[],
         date: new Date(), // When the user arrives, the date is the date of the current date but he can change it from MonthDate component  
         inCommon: [],
+        timeLine: 0,
     }),
 
     mounted(){
       this.loadData();
+      this.setTimeLine();
+      setInterval(this.setTimeLine, 1000*60); // Every minutes, the timeLine is updated
     },
 
     updated(){
@@ -102,8 +108,15 @@ export default{
 
         changeCommon(event){ // We change the people who are in the common list
           this.inCommon = event
-        }
+        },
 
+        setTimeLine(){
+          // extract hour and minutes
+          let date = new Date()
+          let hour = date.getHours()
+          let minutes = date.getMinutes()
+          this.timeLine = hour*60 + minutes
+        }
     }
 }
 
@@ -245,6 +258,9 @@ svg .hour div {
   opacity: 0.5;
 }
 
+#time-actual{
+  border: 1px solid white;
+}
 
 
 @import "../node_modules/bulma/bulma.sass";
